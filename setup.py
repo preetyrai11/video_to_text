70cc9b68bce613d5c49c07926f98fa2a98ee71ca
preetyrai11 <drpreetyrai@gmail.com>
2024-08-15 18:34:11 +0530

    youtub video summarizer code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chatbot',
+    version= '0.0.1',
+    author= 'preetyrai11',
+    author_email= 'drpreetyrai@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
